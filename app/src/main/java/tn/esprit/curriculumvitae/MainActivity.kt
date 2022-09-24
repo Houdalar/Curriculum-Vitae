6ef0e907d9b16661c398aa950bbf6ad240c0daab
houda <houdadalar123@gmail.com>
2022-09-24 11:18:43 +0100

    first step: interface

---
+++ app/src/main/java/tn/esprit/curriculumvitae/MainActivity.kt
@@ -0,0 +1,13 @@
+package tn.esprit.curriculumvitae
+import android.util.Patterns.EMAIL_ADDRESS
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    
+}
